bfb4038c90fea11081c3ddbf999a1e909016f004
Chris Oakes <chris.oakes@outlook.com>
2020-06-12 10:11:43 -0700

    mvp

--- adv.py
+++ adv.py
@@ -1,12 +1,14 @@
 from room import Room
 from player import Player
 from world import World
-
+from util import Stack
 import random
 from ast import literal_eval
 
 # Load world
 world = World()
+
+
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
@@ -30,6 +32,55 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+pathStack = Stack()
+
+visited = set()
+
+totalRooms =len(world.rooms)
+
+
+while len(visited) < totalRooms:  # while loop will go until visited length equals  rooms
+
+    exits = player.current_room.get_exits()
+  
+    path = []
+
+    for x in exits:
+        if  player.current_room.get_room_in_direction(x) not in visited and x:  #if exit exists and we haven't visited
+            
+            path.append(x)
+            
+
+    visited.add(player.current_room) # 0,1
+
+    if len(path) > 0:
+        moveIndex = random.randint(0, len(path) - 1) # this will choose random number  if 0 - 3  n, s, e, w  length dependent 
+        pathStack.push(path[moveIndex])
+        player.travel(path[moveIndex]) #1
+        traversal_path.append(path[moveIndex]) # [1,2,]
+     
+    else:
+        end = pathStack.pop()
+        if end == 'n':
+            end = 's'
+        elif end == 's':
+            end = 'n'
+        elif end == 'e':
+            end = 'w'
+        elif end == 'w':
+            end = 'e'
+        player.travel(end)
+        traversal_path.append(end)
+
+    
+
+
+
+
+
+
+
+
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -51,12 +102,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
